98462e5b0acb7587d78fb84a7acbba3c2514c39d
Dini-is-a-dinasour <DDeSilva@smail.pembinatrails.ca>
2026-06-08 11:49:37 -0500

    trying to push local to new repo

---
+++ Slot_Machine.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "hey there! welcome to my very normal game."
+sleep 2
+echo "This is for entertainment purposes only and this company does not condone gambling in anyway"
+echo "This device may not be used for that purpose and to bet money in anyway."
+sleep 8
+clear
+echo "now that the big boi words are out of the way..."
+sleep 2
+echo "LETS GO GAMBLING"
